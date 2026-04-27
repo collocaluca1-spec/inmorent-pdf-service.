1fb94529f890774415d425bfbd805470021bd4c5
collocaluca1-spec <collocaluca1@gmail.com>
2026-04-27 06:50:33 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingIncludes: {
-    '/api/pdf/locativo': ['./node_modules/@sparticuz/chromium/**/*'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/pdf/locativo': ['./node_modules/@sparticuz/chromium/**/*'],
+    },
   },
 };
 export default nextConfig;
